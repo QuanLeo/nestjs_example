d30e4e13faf22f73251de2bc7a2125a39f5abeed
QuanLeo <quanhn09041996@gmail.com>
2021-09-25 00:13:54 +0700

    Add entity (mysql schema)

--- web/src/coffees/entities/coffee.entity.ts
+++ web/src/coffees/entities/coffee.entity.ts
@@ -1,7 +1,17 @@
 // class output from db
+import { Column, Entity, PrimaryColumn } from "typeorm";
+
+@Entity()  //sql table = cooffee
 export class Coffee {
+  @PrimaryColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   brand: string;
+
+  @Column('json', { nullable: true })
   flavors: string[];
 }
