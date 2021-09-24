edde2c4d6525d00aff64f7750d9a1a6fa76b6434
QuanLeo <quanhn09041996@gmail.com>
2021-09-24 21:07:33 +0700

    Add basic swagger

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Nestjs example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('Coffee')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   const port = process.env.APP_PORT;
   await app.listen(port);
