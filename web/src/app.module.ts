b3bc1cae975e030e8bfa6965d86f502b7d03b104
QuanLeo <quanhn09041996@gmail.com>
2021-09-25 00:05:08 +0700

    Connect to mysql

--- web/src/app.module.ts
+++ web/src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql', // type of our database
+      host: process.env.MYSQL_HOST, // database host
+      port: Number(process.env.MYSQL_PORT), // database port
+      username: process.env.MYSQL_USER, // username
+      password: process.env.MYSQL_PASSWORD, // user password
+      database: process.env.MYSQL_DATABASE, // name of our database,
+      autoLoadEntities: true, // models will be loaded automatically
+      synchronize: true, // your entities will be synced with the database(recommended: disable in prod)
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
