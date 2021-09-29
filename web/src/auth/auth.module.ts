c8d6526badd487531961b4fd1ce04136c8e0dc53
QuanLeo <quanhn09041996@gmail.com>
2021-09-29 23:21:03 +0700

    Setup Jwt config

--- web/src/auth/auth.module.ts
+++ web/src/auth/auth.module.ts
@@ -1,7 +1,16 @@
 import { Module } from '@nestjs/common';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 
 @Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: process.env.APP_SECRET_KEY,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   providers: [AuthService],
 })
 export class AuthModule {}
