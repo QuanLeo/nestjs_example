339a7701b73088129f6f33cdce5777d9a74d0481
QuanLeo <quanhn09041996@gmail.com>
2021-09-29 22:55:15 +0700

    Add TODO for auth-jwt

--- web/src/app.controller.ts
+++ web/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Request } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Request() request: any) {
+    return request.user; //requred Bearer token, validate tokem
+  }
+
+  @Post('login')
+  login(@Request() request: any) {
+    return { msg: 'login success' }; //TODO: return access_token
   }
 }
