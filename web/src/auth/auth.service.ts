7e7206f76c5d6e7bf51dbcfcae8f227b37525915
QuanLeo <quanhn09041996@gmail.com>
2021-09-30 21:14:57 +0700

    Refactor authenticate user with auth_services

--- web/src/auth/auth.service.ts
+++ web/src/auth/auth.service.ts
@@ -1,11 +1,27 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import { BasicAuthDto } from './entities/basic-auth.dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly jwtService: JwtService) {}
+  constructor(
+    private readonly jwtService: JwtService,
+    private readonly userService: UsersService,
+  ) {}
 
-  async login(user: any) {
+  async validateUser(basicAuthDto: BasicAuthDto) {
+    const user = this.userService.findByEmail(basicAuthDto.email);
+
+    if (user && (await user).password === basicAuthDto.password) {
+      return user;
+    }
+
+    throw new UnauthorizedException();
+  }
+
+  async login(basicAuthDto: BasicAuthDto) {
+    const user = await this.validateUser(basicAuthDto);
     const payload = { id: user.id, role: user.role };
     return {
       access_token: this.jwtService.sign(payload),
