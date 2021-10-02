19a6c1283bcbe7ed2e1dd6f0eb4068e315872803
QuanLeo <quanhn09041996@gmail.com>
2021-10-02 09:17:44 +0700

    Test permission basic

--- web/src/app.controller.ts
+++ web/src/app.controller.ts
@@ -10,15 +10,23 @@ import { ApiBearerAuth } from '@nestjs/swagger';
 import { AuthService } from './auth/auth.service';
 import { BasicAuthDto } from './auth/entities/basic-auth.dto';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
+import { CaslAbilityFactory } from './casl/casl-ability.factory';
+import { Action } from './users/action.enum';
 
 @Controller()
 export class AppController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private caslAbilityFactory: CaslAbilityFactory,
+  ) {}
 
   @UseGuards(JwtAuthGuard)
   @ApiBearerAuth('access_token')
   @Get()
   getHello(@Request() request: any) {
+    const ability = this.caslAbilityFactory.createForUser(request.user);
+    const canRead = ability.can(Action.Create, 'all');
+    console.log(canRead);
     return `hello user ${request.user.name}`;
   }
 
